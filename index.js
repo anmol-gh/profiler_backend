63349e072706c69cd49cb15edb31caf6024ad5f3
noob-anmol <anmolghai50@gmail.com>
2023-11-16 01:31:39 +0530

    initialising vercel

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import dribbbleCheck from "./dribbble.js";
 import buyCheck from "./buy.js";
 import cors from "cors";
 const app = express();
-const port = process.env.PORT || 6431;
+const port = process.env.PORT || 8800;
 
 app.use(express.json());
 app.use(cors());
